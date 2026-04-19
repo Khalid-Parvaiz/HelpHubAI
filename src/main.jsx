0c6c714e8534c5f13fb26ab4b1f89fdeaa167de7
Khalid-Parvaiz <khalidparvaiz917@gmail.com>
2026-04-19 05:12:36 +0500

    fix error

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <Routes>
-      <Route path="/helpHubAI" element={<App />} />
+      <Route path="/helpHubAI/" element={<App />} />
       <Route path="/helpHubAI/auth" element={<SignIn />} />
       <Route path="/helpHubAI/auth/signup" element={<SignUp />} />
       <Route path="/helpHubAI/dashboard" element={<Dashboard />} />
